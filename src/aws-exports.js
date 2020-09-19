a9be680328af6983bbb70512de034c051760c76c
TheHumanMachine <Jungleman22@Comcast.net>
2020-09-18 18:00:06 -0700

    Working....

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_ldnbiD3Mj",
     "aws_user_pools_web_client_id": "1295ff35fucfdcron8851jol7j",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://4643pnpgwney5nl2vfbh2bbv24.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ziyzjebqtvhavfevi6ludcrzji",
+    "aws_user_files_s3_bucket": "amplifyapp8d1f2c82249f4582a7d8a8e5619f2886154703-dev",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
